db1d9f9c66a14b7c9470d39baa9f7cd8278966c7
Ben Pope <ben.pope.fla@gmail.com>
2023-10-20 18:05:09 -0700

    fixed bytes parsing in map_state_changes module

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ build:
 
 .PHONY: stream
 stream: build
-	substreams run -e $(ENDPOINT) substreams.yaml map_block -s 12292922 -t +10
+	substreams run -e $(ENDPOINT) substreams.yaml map_state_changes -s 9807005 -t +10
 
 .PHONY: protogen
 protogen:
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod helpers;
 mod pb;
 
 use pb::schema::{Approval, Approvals, Transfer, Transfers, StateChange};
+use substreams::log::println;
 use substreams::pb::substreams::Clock;
 use substreams_entity_change::{pb::entity::EntityChanges, tables::Tables};
 use substreams_ethereum::pb::eth::v2::{StorageChange , Call};
@@ -21,11 +22,14 @@ fn map_state_changes(block: eth::v2::Block) -> Result<StateChange, substreams::e
     let state_changes = block
         .calls()
         .filter_map(|transaction| {
-            if format_hex(&transaction.call.address) == ADDRESS.to_lowercase() && transaction.call.storage_changes.len() > 0 {
-                let mut state_change_data: Vec<(String, String)> = Vec![];
-                for item in transaction.call.storage_changes {
-                    let data = BigInt::from_signed_bytes_be(&item.new_value).to_string();
-                    let slot = BigInt::from_signed_bytes_be(&item.key).to_string();
+            //format_hex(&transaction.call.address) == ADDRESS.to_lowercase() &&
+            if  transaction.call.storage_changes.len() > 0 {
+                let mut state_change_data: Vec<(String, String)> = vec![];
+                for item in &transaction.call.storage_changes  {
+                    let data = BigInt::from_unsigned_bytes_be(&item.new_value).to_string();
+                    let slot = BigInt::from_unsigned_bytes_be(&item.key).to_string();
+                    println(format!("CALL ADDRESS {}", format_hex(&transaction.call.address)));
+                    println(format!("DATA IS: {:?} \n SLOT IS {:?}", &data, &slot));
                     state_change_data.push((slot, data));
                 } 
                 Some(state_change_data)       
@@ -34,84 +38,89 @@ fn map_state_changes(block: eth::v2::Block) -> Result<StateChange, substreams::e
                 None
             }
         })
-        .map(|(transfer, hash)| Transfer {
-            from: format_hex(&transfer.from),
-            to: format_hex(&transfer.to),
-            token_id: transfer.token_id.to_string(),
-            tx_hash: hash,
-        })
-        .collect::<Vec<Transfer>>();
+        .collect::<Vec<_>>();
+        // .map(|state_change_data| { 
+        //     match state_change_data {
 
-    Ok(Transfers { transfers })
+        //     }
+        //     StateChange {
+        //     from: format_hex(&transfer.from),
+        //     to: format_hex(&transfer.to),
+        //     token_id: transfer.token_id.to_string(),
+        //     tx_hash: hash,
+        // }})
+        // .collect::<Vec<Transfer>>();
+
+    Ok(StateChange { ..Default::default() })
 }
 
-fn map_transfers(block: eth::v2::Block) -> Result<Transfers, substreams::errors::Error> {
-    let transfers = block
-        .logs()
-        .filter_map(|log| {
-            if format_hex(log.address()) == ADDRESS.to_lowercase() {
-                if let Some(transfer) = TransferEvent::match_and_decode(log) {
-                    Some((transfer, format_hex(&log.receipt.transaction.hash)))
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        })
-        .map(|(transfer, hash)| Transfer {
-            from: format_hex(&transfer.from),
-            to: format_hex(&transfer.to),
-            token_id: transfer.token_id.to_string(),
-            tx_hash: hash,
-        })
-        .collect::<Vec<Transfer>>();
+// fn map_transfers(block: eth::v2::Block) -> Result<Transfers, substreams::errors::Error> {
+//     let transfers = block
+//         .logs()
+//         .filter_map(|log| {
+//             if format_hex(log.address()) == ADDRESS.to_lowercase() {
+//                 if let Some(transfer) = TransferEvent::match_and_decode(log) {
+//                     Some((transfer, format_hex(&log.receipt.transaction.hash)))
+//                 } else {
+//                     None
+//                 }
+//             } else {
+//                 None
+//             }
+//         })
+//         .map(|(transfer, hash)| Transfer {
+//             from: format_hex(&transfer.from),
+//             to: format_hex(&transfer.to),
+//             token_id: transfer.token_id.to_string(),
+//             tx_hash: hash,
+//         })
+//         .collect::<Vec<Transfer>>();
 
-    Ok(Transfers { transfers })
-}
+//     Ok(Transfers { transfers })
+// }
 
-#[substreams::handlers::map]
-fn map_approvals(block: eth::v2::Block) -> Result<Approvals, substreams::errors::Error> {
-    let approvals = block
-        .logs()
-        .filter_map(|log| {
-            if format_hex(log.address()) == ADDRESS.to_lowercase() {
-                if let Some(approval) = ApprovalEvent::match_and_decode(log) {
-                    Some((approval, format_hex(&log.receipt.transaction.hash)))
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        })
-        .map(|(approval, hash)| Approval {
-            owner: format_hex(&approval.owner),
-            approved: format_hex(&approval.approved),
-            token_id: approval.token_id.to_string(),
-            tx_hash: hash,
-        })
-        .collect::<Vec<Approval>>();
+// #[substreams::handlers::map]
+// fn map_approvals(block: eth::v2::Block) -> Result<Approvals, substreams::errors::Error> {
+//     let approvals = block
+//         .logs()
+//         .filter_map(|log| {
+//             if format_hex(log.address()) == ADDRESS.to_lowercase() {
+//                 if let Some(approval) = ApprovalEvent::match_and_decode(log) {
+//                     Some((approval, format_hex(&log.receipt.transaction.hash)))
+//                 } else {
+//                     None
+//                 }
+//             } else {
+//                 None
+//             }
+//         })
+//         .map(|(approval, hash)| Approval {
+//             owner: format_hex(&approval.owner),
+//             approved: format_hex(&approval.approved),
+//             token_id: approval.token_id.to_string(),
+//             tx_hash: hash,
+//         })
+//         .collect::<Vec<Approval>>();
 
-    Ok(Approvals { approvals })
-}
+//     Ok(Approvals { approvals })
+// }
 
-#[substreams::handlers::map]
-pub fn graph_out(
-    clock: Clock,
-    transfers: Transfers,
-    approvals: Approvals,
-) -> Result<EntityChanges, substreams::errors::Error> {
-    let mut tables = Tables::new();
+// #[substreams::handlers::map]
+// pub fn graph_out(
+//     clock: Clock,
+//     transfers: Transfers,
+//     approvals: Approvals,
+// ) -> Result<EntityChanges, substreams::errors::Error> {
+//     let mut tables = Tables::new();
 
-    if clock.number == START_BLOCK {
-        // Create the collection, we only need to do this once
-        tables.create_row("Collection", ADDRESS.to_string());
-    }
+//     if clock.number == START_BLOCK {
+//         // Create the collection, we only need to do this once
+//         tables.create_row("Collection", ADDRESS.to_string());
+//     }
 
-    transfers_to_table_changes(&mut tables, &transfers);
+//     transfers_to_table_changes(&mut tables, &transfers);
 
-    approvals_to_table_changes(&mut tables, &approvals);
+//     approvals_to_table_changes(&mut tables, &approvals);
 
-    Ok(tables.to_entity_changes())
-}
+//     Ok(tables.to_entity_changes())
+// }
